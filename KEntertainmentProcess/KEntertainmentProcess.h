b63a975803a5df006d42d75c8448bf9b566cd51b
javier.bolanos.ironbit <javier.bolanos@ironbit.com.mx>
2019-12-29 21:03:49 -0600

    Initial Commit

---
+++ KEntertainmentProcess/KEntertainmentProcess.h
@@ -0,0 +1,19 @@
+//
+//  KEntertainmentProcess.h
+//  KEntertainmentProcess
+//
+//  Created by Javier Bolaños on 12/29/19.
+//  Copyright © 2019 Javier Bolaños. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KEntertainmentProcess.
+FOUNDATION_EXPORT double KEntertainmentProcessVersionNumber;
+
+//! Project version string for KEntertainmentProcess.
+FOUNDATION_EXPORT const unsigned char KEntertainmentProcessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KEntertainmentProcess/PublicHeader.h>
+
+
